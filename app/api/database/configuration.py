d11c77caf75c2cbd5e7164067d6c7224fb97f281
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-08-27 20:45:54 -0500

    nt78 feat se crean variables para conectar la base de datos

---
+++ app/api/database/configuration.py
@@ -0,0 +1,14 @@
+#datos para la conexion a la base de datos 
+
+dataBaseName="gestordb"
+userName="root"
+userPassword=""
+connectionPort=3306
+server="localhost"
+
+# creando la conexion 
+dataBaseConnection= f"mysql+mysqlconnector://
+{userName}:{userPassword}@{server}:
+{connectionPort}/{dataBaseName}"
+
+print(dataBaseConnection)
